398d21fb973db98f6d1fd4749d4f8a55c2074adb
ek <ekuchar@seznam.cz>
2014-04-25 23:08:18 +0200

    Comment/usage fixed

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
 ## Matrix inverse of square matrix.
 ## The matrix inverse is cached to prevent repeating time consuming matrix inverse operation.
 ## Usage:
-## 1) Create cached matrix using makeCacheMatrix.
-## 2) When inverse of the matrix is reqiured it is returned by cacheSolve.
-## 3) To get cached matrix use get()
-## 4) To set new cached value use set(matr)
+## 1) Create matrix cache using makeCacheMatrix.
+## 2) Set cached matrix by set() method.
+## 3) When inverse of the matrix is required it is returned by cacheSolve.
+## 4) To get/inspect cached matrix use get()
+## 5) To set another cached matrix use set(matr)
+##
 ## Example:
 ## m <- matrix( c(1,2,3,4), nrow=2, ncol=2) # matrix to be cached/inverted
 ## mCached <- makeCacheMatrix() # create matrix cache
@@ -12,7 +14,7 @@
 ## mCached$get() # get (print) cached matrix
 ## cacheSolve(mCached) %*% m  # get matrix inversion and multiply by orig matrix, unit matrix result
 ## m %*% cacheSolve(mCached)   # multiply orig matrix by its matrix inversion, unit matrix result
-## # The inverse is computed only once, second multiplication uses cached value
+## # The inverse is computed only once, second multiplication uses cached value.
 
 
 ## Create matrix cache
